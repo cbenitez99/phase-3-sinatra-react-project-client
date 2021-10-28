0afbb7a4307747e0dba8450701f7b880dd666121
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-10-28 13:54:57 -0500

    Made get request and connected to db

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from "react"
 import './App.css';
-
+import GameCard from "./components/GameCard"
 function App() {
+  const [games, setGames] = useState([])
+
+  useEffect(()=> {
+    fetch('http://localhost:9292/games')
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data)
+      // setGames(data)
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }, [])
+
   return (
     <div className="App">
-      
+     {games.map((game) => <GameCard game={game}/>)}
     </div>
   );
 }
---
+++ src/components/GameCard.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function GameCard({game}) {
+    return (
+        <div>
+            <p>{game}</p>
+        </div>
+    )
+}
+
+export default GameCard
