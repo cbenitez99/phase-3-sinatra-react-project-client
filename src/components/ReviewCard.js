b02e4a0fad06bb21fb8a3f71e226cbd565afbf1f
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-11-09 22:52:49 -0600

    Just adding features :)

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -3,7 +3,9 @@ import React, {useState} from "react";
 function ReviewCard({review, handleDelete}) {
     
     const [likes, setLikes] = useState(review.likes)
-    
+
+    const [dislikes, setDislikes] = useState(review.likes)
+
     function handlePatch(id) {
         fetch(`http://localhost:9292/reviews/${id}`, {
             method: "PATCH",
@@ -11,8 +13,20 @@ function ReviewCard({review, handleDelete}) {
             body: JSON.stringify({likes: likes + 1 })
         })
         .then(response => response.json())
-        .then(data => setLikes(likes + 1))
+        .then(data => setLikes(likes + 1)) //cause re-render with state, could have been done another way
     };
+
+    function handleDislikePatch(id) {
+        fetch(`http://localhost:9292/reviews/${id}`, {
+            method: "PATCH",
+            headers: {"Content-Type":"application/json"},      
+            body: JSON.stringify({likes: likes - 1 })
+        })
+        .then(response => response.json())
+        .then(data => setDislikes(likes - 1)) //cause re-render with state, could have been done another way
+    };
+
+    
 
     return (
     <div>
@@ -23,7 +37,10 @@ function ReviewCard({review, handleDelete}) {
             <p>Review: <em>"{review.content}"<br/></em>- {review.user_name}</p>
             <div className="card-action" id="button-rev-card">
             <button className="waves-effect waves-dark btn-small" onClick={(e) => handleDelete(review.id)}>Remove Review</button> {""}
-            <button className="waves-effect waves-light btn-small" onClick={() => handlePatch(review.id)}>Like Review: {likes}</button>
+            <button className="waves-effect waves-dark btn-small">{likes ? "Dislike:" : dislikes }</button>
+            <button className="waves-effect waves-light btn-small" onClick={() => handlePatch(review.id)}>{likes ? "Dislike" : "Like"}: {Math.abs(likes)} </button>
+            <button className="waves-effect waves-light btn-small" onClick={() => handleDislikePatch(review.id)}>{dislikes ? "Dislike" : "Like"}: {Math.abs(dislikes)}</button>
+            {/* <button className="waves-effect waves-light btn-small" onClick={() => handlePatch(review.id)}> {likes ? '<3' : null} {likes}</button> */}
             </div>
         </div>
     </div>
